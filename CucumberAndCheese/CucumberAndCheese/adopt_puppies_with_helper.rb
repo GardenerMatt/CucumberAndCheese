0266cacf27282799ee7208b44a7dab82d0c8c30e
Matthew Middleton <middleton.m@gmail.com>
2016-07-10 18:21:37 -0300

    Script to adopt puppies, with a helper script

---
+++ CucumberAndCheese/CucumberAndCheese/adopt_puppies_with_helper.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+require 'watir-webdriver'
+require_relative 'adoption_helper'
+
+include AdoptionHelper
+@browser = Watir::Browser.new :firefox
+goto_puppy_adoption_site
+adopt_puppy_number 1
+continue_adopting_puppies
+adopt_puppy_number 2
+checkout_with('Cheezy', '123 Main St.', 'cheezy@foo.com', 'Check')
+verify_page_contains 'Thank you for adopting a puppy!'
+close_browser
